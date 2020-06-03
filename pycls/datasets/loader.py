ca95a1eb458184c24c8137d1331d1a26d3c4d0d8
ShaoTengLiu <Shaoteng62@outlook.com>
2020-06-03 16:43:30 -0700

    add svhn2mnist

--- pycls/datasets/loader.py
+++ pycls/datasets/loader.py
@@ -17,15 +17,46 @@ from pycls.datasets.imagenet import ImageNet
 from torch.utils.data.distributed import DistributedSampler
 from torch.utils.data.sampler import RandomSampler
 
+from torchvision.datasets import mnist
+from torchvision.datasets import svhn
+from torchvision import transforms
+from torch.utils.data import ConcatDataset
+
+def MNIST(data_path, split, *args, **kwargs):
+    mnist_trans = transforms.Compose([
+                           transforms.Resize(32),
+                           transforms.ToTensor(),
+                           transforms.Normalize((0.1307,), (0.3081,)),
+                           transforms.Lambda(lambda x: x.repeat(3, 1, 1))
+                       ])
+    return mnist.MNIST(data_path, split=='train', mnist_trans, download=True)
+
+def SVHN(data_path, split, *args, **kwargs):
+    svhn_trans = transforms.Compose([
+                        transforms.Resize(32),
+                        transforms.ToTensor(),
+                        transforms.Normalize(
+                            (0.437, 0.4437, 0.4728), 
+                            (0.1980, 0.2010, 0.1970)
+                        ),
+                    ])
+    if split == 'test':
+        return svhn.SVHN(data_path, 'test', svhn_trans, download=True)
+    elif split == 'train':
+        return svhn.SVHN(data_path, 'train', svhn_trans, download=True)
+    elif split == 'train+extra':
+        train_loader = svhn.SVHN(data_path, 'train', svhn_trans, download=True)
+        extra_loader = svhn.SVHN(data_path, 'extra', svhn_trans, download=True)
+        return ConcatDataset((train_loader, extra_loader))
 
 # Supported datasets
-_DATASETS = {"cifar10": Cifar10, "cifar100": Cifar100, "imagenet": ImageNet}
+_DATASETS = {"cifar10": Cifar10, "cifar100": Cifar100, "imagenet": ImageNet, "svhn": SVHN, "mnist": MNIST}
 
 # Default data directory (/path/pycls/pycls/datasets/data)
 _DATA_DIR = os.path.join(os.path.dirname(__file__), "data")
 
 # Relative data paths to default data directory
-_PATHS = {"cifar10": "cifar10", "cifar100": "cifar100", "imagenet": "imagenet"}
+_PATHS = {"cifar10": "cifar10", "cifar100": "cifar100", "imagenet": "imagenet", "svhn": "svhn", "mnist": "mnist"}
 
 
 def _construct_loader(dataset_name, split, corruption_type, corruption_level, batch_size, shuffle, drop_last):

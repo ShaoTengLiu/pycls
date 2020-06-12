a018375e636eb6f8c79b93b8db65d0235c873668
ShaoTengLiu <Shaoteng62@outlook.com>
2020-06-12 14:19:13 -0700

    add w1-8

--- script.sh
+++ script.sh
@@ -1,5 +1,8 @@
-CUDA_VISIBLE_DEVICES=1 \
-    python tools/test_net_ftta.py \
-    --cfg configs/cifar100/R-26_w4_1gpu_adaptbn_entropy.yaml \
-    --corruptions gaussian_noise \
-    --levels 5
+CUDA_VISIBLE_DEVICES=0 \
+python tools/train_net.py \
+    --cfg configs/cifar100/R-26_w8_1gpu_cifar100_bn.yaml
+
+# python tools/test_net_ftta.py \
+# --cfg configs/cifar100/R-26_w4_1gpu_adaptbn_entropy.yaml \
+# --corruptions gaussian_noise \
+# --levels 5

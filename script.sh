2b93da68e9f70e88f367197f57e648f57bc2880b
ShaoTengLiu <Shaoteng62@outlook.com>
2020-06-12 20:09:20 -0700

    add cifar10 w1-w8

--- script.sh
+++ script.sh
@@ -1,6 +1,6 @@
 CUDA_VISIBLE_DEVICES=0 \
 python tools/train_net.py \
-    --cfg configs/cifar100/R-26_w8_1gpu_cifar100_bn.yaml
+    --cfg configs/cifar10/R-26_w8_1gpu_cifar10_bn.yaml
 
 # python tools/test_net_ftta.py \
 # --cfg configs/cifar100/R-26_w4_1gpu_adaptbn_entropy.yaml \

af939aac03a6d19c2876b0921084e4e571abeece
ShaoTengLiu <Shaoteng62@outlook.com>
2020-06-13 15:21:45 -0700

    add oracle

--- script.sh
+++ script.sh
@@ -1,8 +1,8 @@
-CUDA_VISIBLE_DEVICES=0 \
-python tools/train_net.py \
-    --cfg configs/cifar10/R-26_w8_1gpu_cifar10_bn.yaml
+CUDA_VISIBLE_DEVICES=3 \
+    python tools/train_net.py \
+    --cfg configs/cifar100/R-26_w4_1gpu_cifar100_bn_oracle.yaml
 
-# python tools/test_net_ftta.py \
-# --cfg configs/cifar100/R-26_w4_1gpu_adaptbn_entropy.yaml \
-# --corruptions gaussian_noise \
-# --levels 5
+    # python tools/test_net_ftta.py \
+    # --cfg configs/cifar100/R-26_w4_1gpu_adaptbn_entropy.yaml \
+    # --corruptions gaussian_noise \
+    # --levels 5
